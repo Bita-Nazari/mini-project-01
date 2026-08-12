8bd290cd904662d088800c1fdd11abf3160c679b
bita333 <nazaribita33@gmail.com>
2026-08-13 00:54:38 +0330

    feat: add models and train them

--- src/train.py
+++ src/train.py
@@ -13,3 +13,16 @@ X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
 joblib.dump(scaler,'./models/scaler.pkl')
+
+logistic_model = LogisticRegression()
+logistic_model.fit(X_train_scaled,y_train)
+y_pred_logistic = logistic_model.predict(X_test_scaled)
+
+
+knn_model = KNeighborsClassifier(n_neighbors=5)
+knn_model.fit(X_train_scaled,y_train)
+y_pred_knn = knn_model.predict(X_test_scaled)
+
+decision_tree_model = DecisionTreeClassifier(max_depth=6)
+decision_tree_model.fit(X_train_scaled,y_train)
+y_decision_tree= decision_tree_model.predict(X_test_scaled)
